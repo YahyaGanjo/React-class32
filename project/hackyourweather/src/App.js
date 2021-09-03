f1376ef89b902aee8c3ded3bc7976f3c5c13ce5c
Yahya Ganjo <yahya.ganjo@gmail.com>
2021-09-03 09:09:26 +0200

    feedback implemented

--- project/hackyourweather/src/App.js
+++ project/hackyourweather/src/App.js
@@ -7,21 +7,19 @@ function App() {
     <div className="App">
       <h1>Weather</h1>
       <div className="container">
-        {cities.map((city) => {
-          return (
-            <Card
-              key={city.id}
-              name={city.name}
-              country={city.sys.country}
-              weatherMain={city.weather[0].main}
-              description={city.weather[0].description}
-              minTemp={city.main.temp_min}
-              maxTemp={city.main.temp_max}
-              lon={city.coord.lon}
-              lat={city.coord.lat}
-            />
-          );
-        })}
+        {cities.map((city) => (
+          <Card
+            key={city.id}
+            name={city.name}
+            country={city.sys.country}
+            weatherMain={city.weather[0].main}
+            description={city.weather[0].description}
+            minTemp={city.main.temp_min}
+            maxTemp={city.main.temp_max}
+            lon={city.coord.lon}
+            lat={city.coord.lat}
+          />
+        ))}
       </div>
     </div>
   );
